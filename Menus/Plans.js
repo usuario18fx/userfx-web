1877e91d8006c6d9124fcb89ec203b737e6bc88e
User Fx <234803878+usuario18fx@users.noreply.github.com>
2026-03-10 16:38:04 -0400

    Create Plans.js
    Ok

---
+++ Menus/Plans.js
@@ -0,0 +1,10 @@
+import { InlineKeyboard } from "grammy";
+
+export function getPlansMenu() {
+  return new InlineKeyboard()
+    .text("☁️ Cloud Básico - 5 USD", "buy_plan_basic")
+    .row()
+    .text("👑 Cloud VIP Pro - 15 USD", "buy_plan_premium")
+    .row()
+    .text("⬅️ Volver al Panel", "back_to_main");
+}
