cd10319ad14b6f30c5bbcd3c38fd0fff48ef3c5f
User Fx <234803878+usuario18fx@users.noreply.github.com>
2026-03-15 08:58:21 -0400

    Crear db.js
    Ok

---
+++ db.js
@@ -0,0 +1,9 @@
+import "dotenv/config";
+import pg from "pg";
+
+const { Pool } = pg;
+
+export const db = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+});
