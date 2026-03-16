caab0b978fe2bbc3b5512cb823310b22626bc435
User Fx <234803878+usuario18fx@users.noreply.github.com>
2026-03-16 01:37:52 -0400

    Delete db.js
    ok

--- db.js
+++
@@ -1,9 +0,0 @@
-import "dotenv/config";
-import pg from "pg";
-
-const { Pool } = pg;
-
-export const db = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
-});
