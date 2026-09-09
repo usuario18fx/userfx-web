58d6a9189071fdf76cef91258512092ab762dde2
User Fx <234803878+usuario18fx@users.noreply.github.com>
2026-09-09 02:58:03 -0400

    load access modal visibility overrides

--- components/VaultActionsBtns/VaultActions.tsx
+++ components/VaultActionsBtns/VaultActions.tsx
@@ -6,6 +6,7 @@ import "./VaultActions.css";
 import "./VaultMobileCenter.css";
 import "./VaultIdentity.css";
 import "./VaultMobilePolish.css";
+import "../FxAccess/FxAccessModal/FxAccessModalVisibility.css";
 
 import { FxAccessBtn } from "../FxAccess/FxAccessBtn";
 import { FxAccessModal } from "../FxAccess/FxAccessModal/FxAccessModal";
