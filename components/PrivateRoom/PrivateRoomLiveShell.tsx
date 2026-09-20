82979f5aa31c8c92d9a187acc597c27612ad0896
User Fx <234803878+usuario18fx@users.noreply.github.com>
2026-09-20 11:10:49 -0400

    Load final camera overlay controls

--- components/PrivateRoom/PrivateRoomLiveShell.tsx
+++ components/PrivateRoom/PrivateRoomLiveShell.tsx
@@ -7,6 +7,7 @@ import PrivateRoomWelcomeTour from "./PrivateRoomWelcomeTour";
 import "./PrivateRoomLuxury.css";
 import "./PrivateRoomLatestUpdate.css";
 import "./PrivateRoomLayoutGuard.css";
+import "./PrivateRoomCameraOverlay.css";
 
 export default function PrivateRoomLiveShell() {
   return (
