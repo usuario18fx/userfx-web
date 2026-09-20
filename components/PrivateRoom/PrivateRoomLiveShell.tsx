2081df695a122c14fb77f31b5eda7c2ef3d73c8b
User Fx <234803878+usuario18fx@users.noreply.github.com>
2026-09-20 00:43:30 -0400

    feat: mount live lobby into private room

---
+++ components/PrivateRoom/PrivateRoomLiveShell.tsx
@@ -0,0 +1,11 @@
+import PrivateRoomAccount from "./PrivateRoomAccount";
+import PrivateRoomLiveLobby from "./PrivateRoomLiveLobby";
+
+export default function PrivateRoomLiveShell() {
+  return (
+    <>
+      <PrivateRoomAccount />
+      <PrivateRoomLiveLobby />
+    </>
+  );
+}
