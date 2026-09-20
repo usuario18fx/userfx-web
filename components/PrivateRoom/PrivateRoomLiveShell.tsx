924c17ea901186599c7297cdda7812ad140b9f1a
User Fx <234803878+usuario18fx@users.noreply.github.com>
2026-09-20 02:56:42 -0400

    Use luxury private club shell

--- components/PrivateRoom/PrivateRoomLiveShell.tsx
+++ components/PrivateRoom/PrivateRoomLiveShell.tsx
@@ -1,9 +1,12 @@
+import PrivateRoomLuxuryNav from "./PrivateRoomLuxuryNav";
 import PrivateRoomAccount from "./PrivateRoomAccount";
 import PrivateRoomLiveLobby from "./PrivateRoomLiveLobby";
+import "./PrivateRoomLuxury.css";
 
 export default function PrivateRoomLiveShell() {
   return (
     <>
+      <PrivateRoomLuxuryNav />
       <PrivateRoomAccount />
       <PrivateRoomLiveLobby />
     </>
