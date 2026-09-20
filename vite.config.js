2592b5bb556df48b635690c307fce42bee3f37eb
User Fx <234803878+usuario18fx@users.noreply.github.com>
2026-09-20 04:23:19 -0400

    prefer tsx private room implementation over legacy jsx

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    extensions: [".mjs", ".js", ".mts", ".ts", ".tsx", ".jsx", ".json"],
+  },
 });
